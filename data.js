992e5849d17fa3100598f3b8accab83acc149f90
Pedroca <pedroernestopgoncalves@gmail.com>
2024-09-06 13:45:35 -0400

    pesquisa otimizada

--- data.js
+++ data.js
@@ -7,21 +7,21 @@ let listaCarros = [
         origem : "Japão",
         descricao : "O Civic é um dos modelos mais icônicos e populares da Honda, conhecido por sua confiabilidade, economia de combustível e design atemporal. Desde seu lançamento, o Civic conquistou uma legião de fãs ao redor do mundo, e sua versatilidade o tornou uma base popular para modificações. Para mais informações acesse o link acima.",
         link : "https://www.youtube.com/watch?v=KYoIEBHv-tg&pp=ygUPYWNmIGhvbmRhIGNpdmlj",
-        marcadores : "jdm carro"
+        marcadores : "jdm carro civic honda Civic Honda"
     },{
         fabricante : "Chevrolet",
         modelo : "Chevette",
         origem : "Brasil",
         descricao : "O Chevrolet Chevette foi mais do que um carro; ele foi um marco na história automobilística brasileira. Lançado em 1973 pela Chevrolet, o modelo se tornou rapidamente um dos mais populares do país, marcando gerações e deixando saudade em muitos corações. Para mais informações acesse o link acima.",
         link : "https://www.youtube.com/watch?v=qrrIXKU8l_I&pp=ygUSYWNmIGNoZXZldHRlIHR1cmJv",
-        marcadores : "drift carro"
+        marcadores : "drift carro chevette Chev"
     },{
         fabricante : "Chevrolet",
         modelo : "Opala",
         origem : "Brasil",
         descricao : "O Chevrolet Opala é, sem dúvida, um dos carros mais emblemáticos da indústria automobilística brasileira. Produzido pela General Motors do Brasil entre 1968 e 1992, o Opala conquistou o coração dos brasileiros e deixou um legado que perdura até hoje. Para mais informações acesse o link acima.",
         link : "https://www.youtube.com/watch?v=4csLF3nFf-4&pp=ygUJYWNmIG9wYWxh",
-        marcadores : "acelero carro"
+        marcadores : "acelero carro opala Opala"
     },{
         fabricante : "Volkswagen",
         modelo : "Gol",
@@ -35,6 +35,6 @@ let listaCarros = [
         origem : "Europa e LATAM",
         descricao : "O Fiat Uno é, sem dúvida, um dos carros mais icônicos da história automobilística brasileira. Lançado em 1984, ele rapidamente conquistou o coração dos brasileiros por sua simplicidade, robustez e economia. Para mais informações acesse o link acima.",
         link : "https://www.youtube.com/watch?v=4LuMqj9Sg2M&pp=ygUHYWNmIHVubw%3D%3D",
-        marcadores : "escada carro"
+        marcadores : "escada carro uno fiat Uno Fiat"
     }
 ];
