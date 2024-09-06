d7be662e3ab1cb4d247da48309a73f42d8f8c80c
Pedroca <pedroernestopgoncalves@gmail.com>
2024-09-06 13:19:05 -0400

    função pesquisar() devidamente implementada

--- main.js
+++ main.js
@@ -3,19 +3,38 @@ function pesquisar() {
 
     let resultados = "";
 
-    for (let listObject of listaCarros) {
-        resultados += `
-            <div class="item-resultado">
-                    <h2>
-                        <a href="${listObject.link}" target="_blank">${listObject.fabricante} ${listObject.modelo}</a>
-                    </h2>
-                    <p class="descricao-meta">${listObject.descricao}</p>
-            </div>
-        `;
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p>Nenhum resultado...</p>"
+        return
     }
 
-    
+    campoPesquisa = campoPesquisa.toLowerCase();
 
+    let modelo = "";
+    let fabricante = "";
+    let descricao = "";
+
+
+    for (let listObject of listaCarros) {
+        if (listObject.modelo.includes(campoPesquisa) || listObject.fabricante.includes(campoPesquisa) || listObject.descricao.includes(campoPesquisa) || listObject.marcadores.includes(campoPesquisa)){
+            resultados += `
+                <div class="item-resultado">
+                        <h2>
+                            <a href="${listObject.link}" target="_blank">${listObject.fabricante} ${listObject.modelo}</a>
+                        </h2>
+                        <p class="descricao-meta">${listObject.descricao}</p>
+                </div>
+            `;
+
+        }
+    }
+
+    if (!resultados) {
+        resultados = "Nenhum resultado..."
+    }
 
     section.innerHTML = resultados;    
 }
